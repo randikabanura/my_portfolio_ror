e513ecd2006b2956290f7bf7214d5e71f96d70ef
Banura Randika <randika.banura@gmail.com>
2020-05-10 00:36:07 +0530

    Home page add

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,24 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
+require("bootstrap");
+require("popper.js")
+require('custom/jquery.waypoints.js')
+require("custom/mbclicker.min.js")
+require("custom/menu")
+require("custom/mobile-menu")
+require("custom/portfolio-filter")
+//require("custom/progress-list")
+//require("custom/section")
+require("custom/site-btn")
+require("custom/slider-carousel")
+require("custom/style-switcher")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free";
+import "@fortawesome/fontawesome-free/js/all";
